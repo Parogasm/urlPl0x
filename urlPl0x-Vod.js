e5a22e8ee221ef810735a28a455a538e4ff73492
Parog <ult_fuzzyboy@hotmail.com>
2015-11-01 00:30:47 -0400

    Download Link Pl0x
    Initial Push

---
+++ urlPl0x-Vod.js
@@ -0,0 +1,6 @@
+// Made by Parog of OwnedCore.com 
+// This script is for educational purposes only.
+
+var videoUrl = $("input[name~='videourl']").val()
+
+$("#file_title").append('<a href="' + videoUrl + '">***Download***</a>')
