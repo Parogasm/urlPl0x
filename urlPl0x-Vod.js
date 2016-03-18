c49a2eaa904ada9eb3907144c5cc3a3708435f3f
Parog <ult_fuzzyboy@hotmail.com>
2016-03-18 02:26:32 -0400

    Fixed Script
    Made it search for text instead of specifying jquery selector.

--- urlPl0x-Vod.js
+++ urlPl0x-Vod.js
@@ -1,6 +1,6 @@
 // Made by Parog of OwnedCore.com 
 // This script is for educational purposes only.
 
-var videoUrl = $("input[name~='videourl']").val()
+var videoUrl = $("#player_code > script:nth-child(3)").html().match(/file: "(\S+)"/)[1]
 
 $("#file_title").append('<a href="' + videoUrl + '">***Download***</a>')
