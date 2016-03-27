87cc4c0a166953fca1fd9a20eed5405695c7159a
Parog <ult_fuzzyboy@hotmail.com>
2016-03-26 22:29:07 -0400

    Added auto-download
    Removed the ***Download*** area. Will now automatically download once
    the script is ran.

--- urlPl0x-Vod.js
+++ urlPl0x-Vod.js
@@ -1,6 +1,8 @@
 // Made by Parog of OwnedCore.com 
 // This script is for educational purposes only.
 
-var videoUrl = $("#player_code > script:nth-child(3)").html().match(/file: "(\S+)"/)[1]
+var videoUrl = $("#player_code > script:nth-child(3)").html().match(/file: "(\S+)"/)[1];
+var a = $("<a>").attr("href", videoUrl).attr("download", videoUrl).appendTo("body"); 
 
-$("#file_title").append('<a href="' + videoUrl + '">***Download***</a>')
+a[0].click(); 
+a.remove(); 
