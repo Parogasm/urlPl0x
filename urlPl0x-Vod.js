e8ba03f63bb16215b8b28abcde95b3899d4e22da
Parog <ult_fuzzyboy@hotmail.com>
2016-05-11 23:18:19 -0400

    Fixed script
    Broadened scope

--- urlPl0x-Vod.js
+++ urlPl0x-Vod.js
@@ -1,7 +1,7 @@
 // Made by Parog of OwnedCore.com 
 // This script is for educational purposes only.
 
-var videoUrl = $("#player_code > script:nth-child(3)").html().match(/file: "(\S+)"/)[1];
+var videoUrl = $("#player_code").html().match(/file: "(\S+)"/)[1];
 var a = $("<a>").attr("href", videoUrl).attr("download", videoUrl).appendTo("body"); 
 
 a[0].click(); 
